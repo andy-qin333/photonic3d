914e8e403ee10fee4cd7bd43e2e9d92c0615c443
dbtzjx <dbtzjx@gmail.com>
2020-08-11 17:39:54 +0800

    测试版 修改白屏速度过快导致曝光的问题

--- Photonic3D-1.0.15/host/src/main/java/org/area515/resinprinter/job/AbstractPrintFileProcessor.java
+++ Photonic3D-1.0.15/host/src/main/java/org/area515/resinprinter/job/AbstractPrintFileProcessor.java
@@ -683,7 +683,7 @@ public abstract class AbstractPrintFileProcessor<G,E> implements PrintFileProces
 
 		//Blank the screen
 		aid.printer.showBlankImage();
-		aid.printer.showWhiteImage();
+		
 		// FIXME: 2017/9/15 zyd add for set delay time -s
 		if (aid.slicingProfile.getDelayTimeAfterSolidify() > 0)
 		{
@@ -720,7 +720,7 @@ public abstract class AbstractPrintFileProcessor<G,E> implements PrintFileProces
 		// FIXME: 2017/9/25 zyd add for parameter -e
 		//Perform the lift feed gcode manipulation
 		aid.printer.getGCodeControl().executeGCodeWithTemplating(aid.printJob, aid.slicingProfile.getgCodeLiftFeed(), false);
-		
+		aid.printer.showWhiteImage();
 		needPerformAfterPause = false;
 		while (true)
 		{
