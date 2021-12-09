18cec5ddc1110d22bac3119c88592d56599db2c8
dbtzjx <dbtzjx@gmail.com>
2021-12-09 12:06:49 +0800

    V0.4.42 1、恢复SimpleImageRenderer函数，不必为png格式处理像素

--- Photonic3D-1.0.15/host/src/main/java/org/area515/resinprinter/twodim/SimpleImageRenderer.java
+++ Photonic3D-1.0.15/host/src/main/java/org/area515/resinprinter/twodim/SimpleImageRenderer.java
@@ -19,24 +19,7 @@ public class SimpleImageRenderer extends CurrentImageRenderer {
 	@Override
 	public BufferedImage renderImage(BufferedImage image) throws JobManagerException {
 		try {
-//			return ImageIO.read((File)imageIndexToBuild);
-			////modified by derby for mono screen using png files
-			BufferedImage imgSource = ImageIO.read((File)imageIndexToBuild);
-			BufferedImage monoImage = new BufferedImage(aid.xResolution/3, aid.yResolution, BufferedImage.TYPE_3BYTE_BGR);
-            for(int i = 0; i < monoImage.getHeight(); i++) {
-            	for(int j = 0; j < monoImage.getWidth(); j++) {
-            		int pix0 = imgSource.getRGB(j*3+2, i);
-            		int pix1 = imgSource.getRGB(j*3+1,i );
-            		int pix2 = imgSource.getRGB(j*3,i );
-            		pix0 = pix0&0x000000FF;
-            		pix1 = pix1&0x0000FF00;
-            		pix2 = pix2&0x00FF0000;
-            		int monoPix = pix0 + pix1 + pix2;                		
-            		monoPix = monoPix + 0xFF000000;
-            		monoImage.setRGB(j, i, monoPix);
-            	}
-            }
-            return monoImage;
+			return ImageIO.read((File)imageIndexToBuild);
 		} catch (IOException e) {
 			throw new JobManagerException("Unable to read image:" + imageIndexToBuild, e);
 		}
